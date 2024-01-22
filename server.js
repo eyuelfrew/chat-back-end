8c7b5ddb0a869712bb7b9311e966eba6b2e8ccbf
Eyuel Frew <eyuelfrew430@gmail.com>
2024-01-22 19:39:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ import { notFound } from "./middleware/errorMiddleware.js";
 import { errorHandler } from "./middleware/errorMiddleware.js";
 import cors from "cors";
 const corsOptions = {
-  origin: ["https://chat-front-end-three.vercel.app"],
+  origin: ["https://chat-front-end-three.vercel.app/"],
   methods: ["POST", "PUT", "DELETE", "GET"],
   credentials: true,
 };
